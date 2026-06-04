4a651d352681dcd2e4c221fbc00d1b4731a21cca
吴江泉(Jiangquan Wu) <qmagician@qq.com>
2026-06-04 15:39:52 +0800

    更新api请求路径

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -40,7 +40,7 @@ export class Timer {
 export const myFetch = $fetch.create({
   timeout: 15000,
   retry: 0,
-  baseURL: `${import.meta.env.BASE_PATH || ""}/api`,
+  baseURL: `${import.meta.env.VITE_BASE_PATH || import.meta.env.BASE_PATH || ""}/api`,
 })
 
 export function isiOS() {
